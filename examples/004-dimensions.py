82d76f880341b5283789c41e83d7bcc10111dca9
igo95862 <igo95862@yandex.ru>
2020-10-18 17:11:00 +0300

    Added example with dimensions

---
+++ examples/004-dimensions.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright 2020 igo95862
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from time import sleep
+
+from notcurses.notcurses_context import NotcursesContext
+
+ncontx = NotcursesContext()
+
+std_plane = ncontx.get_std_plane()
+
+red = 0x80
+green = 0x80
+blue = 0x80
+
+y_dimension, x_dimension = std_plane.get_dimensions()
+
+for y in range(y_dimension):
+    for x in range(x_dimension):
+        std_plane.set_foreground_color(red, green, blue)
+        std_plane.set_background_color(red, green, blue)
+        std_plane.putstr('X', y_pos=y, x_pos=x)
+        blue = (blue + 2) % 256
+        green = (green + 2) % 256
+        red = (red + 2) % 256
+
+ncontx.render()
+
+sleep(5)
